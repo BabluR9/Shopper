48db45805db20be3833c467495bdabf3503717cb
BabluR9 <babluamulya1998@gmail.com>
2024-04-10 18:38:07 +0530

    Intial App added

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-navbox">Description</div>
+        <div className="descriptionbox-navbox fade">Reviews(108)</div>
+      </div>
+      <div className="descrptionbox-description">
+        <p>An ecommerce website is your digital storefront on the internet. It facilitates the transaction between a buyer and seller. It is the virtual space where you showcase products, and online customers make selections. Your website acts as the product shelves, sales staff, and cash register of your online business channel.</p>
+        <p>E-commerce content is the list of various materials intended to attract potential buyers. Brands create content to present information through many channels and mediums.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
